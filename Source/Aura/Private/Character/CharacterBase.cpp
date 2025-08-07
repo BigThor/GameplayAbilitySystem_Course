ad25e26294e360e4eed3bf2891936e4bc35f18d1
Víctor Adrián Suárez Ruiz <jr._5oct1998@hotmail.com>
2025-08-06 20:33:03 -0600

    Add Player and Enemy character classes

---
+++ Source/Aura/Private/Character/CharacterBase.cpp
@@ -0,0 +1,16 @@
+// Copyright Big Thor
+
+
+#include "Character/CharacterBase.h"
+
+ACharacterBase::ACharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+// Called when the game starts or when spawned
+void ACharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
---
+++ Source/Aura/Private/Character/PlayerCharacter.cpp
@@ -0,0 +1,5 @@
+// Copyright Big Thor
+
+
+#include "Character/PlayerCharacter.h"
+
